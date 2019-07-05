bdf6765bf798a265fc7cc96fdb766a29340bef55
Yuri Fedorov <yuri.inpost@yandex.ru>
2019-07-05 17:42:27 +0300

    content(src): edit

--- src/templates/examples/javascript/apps/filters/simplest_filter_render_function.js
+++ src/templates/examples/javascript/apps/filters/simplest_filter_render_function.js
@@ -1,10 +1,9 @@
-let list = ['JavaScript','Kotlin','Rust','PHP','Ruby','Java','MarkDown','Python','C++','Fortran','Assembler']
-const result = document.getElementById('results')
-renderList(list,result)
 function renderList(_list=[],el=document.body){
+	el.innerHTML='';
   _list.forEach(i=>{
     let new_el = document.createElement('li')
     new_el.innerHTML=i
     el.appendChild(new_el)
   })
+  console.timeEnd('test')
 }
