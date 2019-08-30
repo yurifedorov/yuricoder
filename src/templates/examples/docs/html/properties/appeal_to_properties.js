f0d50a62230e1f2d234df3672803bd0358b7580a
Yuri Fedorov <yuri.inpost@yandex.ru>
2019-08-30 10:05:25 +0300

    content(src): edit

---
+++ src/templates/examples/docs/html/properties/appeal_to_properties.js
@@ -0,0 +1,7 @@
+let logoImg = document.querySelector('#logo'); // получим элемент
+
+// выведем в консоль DOM-свойства элемента
+console.log(logoImg.id); // "brand"
+console.log(logoImg.className); // "brand"
+console.log(logoImg.src); // "logo.png"
+console.log(logoImg.alt); // "логотип сайта"
