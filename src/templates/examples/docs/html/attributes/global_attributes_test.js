b72388d0c9d7e85f0cb41bcf0c549aae71d3d5cb
Yuri Fedorov <yuri.inpost@yandex.ru>
2019-09-02 17:16:21 +0300

    content(src): edit

---
+++ src/templates/examples/docs/html/attributes/global_attributes_test.js
@@ -0,0 +1,16 @@
+let elem = document.querySelector('#ok');
+
+console.log(elem.hasAttribute('accesskey'));
+console.log(elem.hasAttribute('class'));
+console.log(elem.hasAttribute('contenteditable'));
+console.log(elem.hasAttribute('dir'));
+console.log(elem.hasAttribute('draggable'));
+console.log(elem.hasAttribute('hidden'));
+console.log(elem.hasAttribute('id'));
+console.log(elem.hasAttribute('lang'));
+console.log(elem.hasAttribute('spellcheck'));
+console.log(elem.hasAttribute('style'));
+console.log(elem.hasAttribute('tabindex'));
+console.log(elem.hasAttribute('title'));
+console.log(elem.hasAttribute('translate'));
+console.log(elem.hasAttribute('non-standart-attr')); // false
