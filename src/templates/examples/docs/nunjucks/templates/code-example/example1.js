6d971a4e6855a995fe141d4975589af40642d52d
Yuri Fedorov <yuri.inpost@yandex.ru>
2019-10-08 16:37:01 +0300

    content(src): edit

--- src/templates/examples/docs/nunjucks/templates/code-example/example1.js
+++ src/templates/examples/docs/nunjucks/templates/code-example/example1.js
@@ -1,10 +1,12 @@
 // javascript (example-1)
 function compareNumeric(a, b) {
- if (a > b) return 1;
- if (a == b) return 0;
- if (a < b) return -1;
+  if (a > b) return 1;
+  if (a == b) return 0;
+  if (a < b) return -1;
 }
 
 let arr = [ 1, 2, 15 ];
 
 arr.sort(compareNumeric);
+
+alert(arr);  // 1, 2, 15
